b18047c6de20949478eb1400de342a5c6d4934ef
Developing Human <developinghuman@protonmail.com>
2025-11-17 19:34:41 -0500

    add waybar to setup.sh

--- setup.sh
+++ setup.sh
@@ -12,6 +12,10 @@ ln -s `pwd`/nvim ~/.config/nvim
 mv ~/.config/hypr ~/.config/hypr.bak
 ln -s `pwd`/hypr ~/.config/hypr
 
+# Link waybar config
+mv ~/.config/waybar ~/.config/waybar.bak
+ln -s `pwd`/waybar ~/.config/waybar
+
 # Link dunst config
 mv ~/.config/dunst ~/.config/dunst.bak
 ln -s `pwd`/dunst ~/.config/dunst
