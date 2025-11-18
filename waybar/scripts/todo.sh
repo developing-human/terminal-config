3e86b2b29913271dbddd204521fa043c48f6e7d3
Developing Human <developinghuman@protonmail.com>
2025-11-18 17:22:23 -0500

    add docs to todo.sh

--- waybar/scripts/todo.sh
+++ waybar/scripts/todo.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+# Processes a very simple todo list, where todos are lines
+# that start with a hyphen. Indented subtasks aren't included
+# in the count.
+
+# Filter to lines that start with a hyphen.
 COUNT=$(cat ~/todo.txt | grep "^-" | wc -l)
+
+# The tooltip includes subtasks.
 TODOS=$(cat ~/todo.txt | grep "^\s*-" | head -c -1 - | sed -z 's/\n/\\n/g')
+
 printf '{"text": "%s", "tooltip": "%s"}\n' "$COUNT" "$TODOS"
