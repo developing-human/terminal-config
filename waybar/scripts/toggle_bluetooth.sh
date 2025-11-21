48146dbd33e7adac02f8b3a08766aa34fc6e22c1
Developing Human <developinghuman@protonmail.com>
2025-11-21 11:58:33 -0500

    toggle bluetooth on, even from off-blocked state

--- waybar/scripts/toggle_bluetooth.sh
+++ waybar/scripts/toggle_bluetooth.sh
@@ -3,5 +3,8 @@
 if bluetoothctl show | grep -q "Powered: yes"; then
     bluetoothctl power off
 else
+    # occasionally the bluetooth controller gets into the state
+    # PowerState: off-blocked, and this unblocks it.
+    rfkill unblock 0
     bluetoothctl power on
 fi
