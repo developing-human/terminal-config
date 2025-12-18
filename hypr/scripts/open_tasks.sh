622de438275cbd736185b865b4e195db58977ccb
Developing Human <developinghuman@protonmail.com>
2025-12-18 12:10:33 -0500

    switch to taskwarrior for tasks

---
+++ hypr/scripts/open_tasks.sh
@@ -0,0 +1,1 @@
+alacritty --class tasks -e tmux attach -t tasks \; send-keys -t tasks:0.0 'clear && task' C-m
---
+++ waybar/scripts/next_task.sh
@@ -0,0 +1,1 @@
+task _get `task next limit:1 | head -4 | tail -1 | cut -d' ' -f 2`.description
